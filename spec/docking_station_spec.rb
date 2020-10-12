0507292b63c722d8f2cfd123219ae545849c47d6
Nia John <niajohn416@gmail.com>
2020-10-12 15:26:13 +0100

    Created unit test and release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking-station'
 
 describe DockingStation do
-  
+  # it "DockingStation has a release method" do
+  #   expect(subject).to respond_to(:release_bike)
+  # end
+  it {expect respond_to(:release_bike)}
 end
